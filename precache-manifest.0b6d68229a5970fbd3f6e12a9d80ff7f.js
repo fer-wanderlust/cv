866f2524cd8c1624cb948377e05534439108a568
fer <ferconde87@gmail.com>
2020-03-22 16:33:14 +0700

    Updates

---
+++ precache-manifest.0b6d68229a5970fbd3f6e12a9d80ff7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd47cf2b98c0d31b51ed6e13a83ab83",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "3fbec3d6f46745302037",
+    "url": "/cv/static/css/main.e8ef7aef.chunk.css"
+  },
+  {
+    "revision": "89d1d24da6b55c2ba8f7",
+    "url": "/cv/static/js/2.36713a5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.36713a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fbec3d6f46745302037",
+    "url": "/cv/static/js/main.7ea48041.chunk.js"
+  },
+  {
+    "revision": "37e5bded343fadbbe3aa",
+    "url": "/cv/static/js/runtime-main.65e32292.js"
+  }
+]);
